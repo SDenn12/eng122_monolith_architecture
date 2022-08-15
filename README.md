828dd9efcee8a6b964ed65a5e370931c1e63b421
SDenn12 <samuel.dennis0@gmail.com>
2022-08-15 16:12:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Monolith Architecture 
+
+A monolithic artchitecture is the traditional unified model for the design of a software program.
+
+![](mono_image.png)
---
+++ provision.sh
@@ -0,0 +1,17 @@
+# updates ubuntu
+sudo apt-get update
+
+sudo apt-get upgrade -y
+
+# nginx install
+sudo apt-get install nginx -y
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+# nodejs install
+sudo apt-get purge nodejs npm
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# pm2 install
+sudo npm install pm2
